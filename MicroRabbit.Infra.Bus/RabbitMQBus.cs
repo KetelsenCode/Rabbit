3810a2f406997006966001d4d47a522fd5ce7c22
KetelsenCode <pardonmychessmate@gmail.com>
2019-07-30 14:13:47 +0200

    Publish/Subscribe created

--- MicroRabbit.Infra.Bus/RabbitMQBus.cs
+++ MicroRabbit.Infra.Bus/RabbitMQBus.cs
@@ -4,8 +4,10 @@ using MicroRabbit.Domain.Core.Commands;
 using MicroRabbit.Domain.Core.Events;
 using Newtonsoft.Json;
 using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -16,7 +18,7 @@ namespace MicroRabbit.Infra.Bus
     {
         private readonly IMediator _mediator;
         private readonly Dictionary<string, List<Type>> _handlers;
-        private readonly List<Type> _eventTypes;
+        private readonly List<Type> _eventTypes; //List of all types of events
 
         public RabbitMQBus(IMediator mediator)
         {
@@ -57,7 +59,92 @@ namespace MicroRabbit.Infra.Bus
             where T : Event
             where TH : IEventHandler<T>
         {
-            throw new NotImplementedException();
+            var eventName = typeof(T).Name;
+            var handlerType = typeof(TH);
+
+            //Checks if our eventTypes contains the specific type of event, if not it adds it to our list
+            if (!_eventTypes.Contains(typeof(T)))
+            {
+                _eventTypes.Add(typeof(T));
+            }
+
+            //Checks if we already have the event - in our examlpe TransferEvent gets added to our handler
+            if (!_handlers.ContainsKey(eventName))
+            {
+                _handlers.Add(eventName, new List<Type>());
+            }
+
+            //Throws exception if we have any event
+            if (_handlers[eventName].Any(s => s.GetType() == handlerType))
+            {
+                throw new ArgumentException($"Handler Type {handlerType.Name} already is registred for '{eventName}'", nameof(handlerType));
+            }
+
+            _handlers[eventName].Add(handlerType);
+
+            StartBasicConsume<T>();
+        }
+
+        private void StartBasicConsume<T>() where T : Event
+        {
+            var factory = new ConnectionFactory()
+            {
+                HostName = "localhost",
+                DispatchConsumersAsync = true
+            };
+
+            var connection = factory.CreateConnection();
+            var channel = connection.CreateModel();
+
+            var eventName = typeof(T).Name;
+
+            channel.QueueDeclare(eventName, false, false, false, null);
+
+            var consumer = new AsyncEventingBasicConsumer(channel);
+
+            //Listens for messages from ueue
+            consumer.Received += Consumer_Received;
+
+            channel.BasicConsume(eventName, true, consumer);
+
+        }
+
+        private async Task Consumer_Received(object sender, BasicDeliverEventArgs @event)
+        {
+            var eventName = @event.RoutingKey;
+            var message = Encoding.UTF8.GetString(@event.Body);
+
+            try
+            {
+                await ProcessEvent(eventName, message).ConfigureAwait(false)
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+
+        }
+
+        private async Task ProcessEvent(string eventName, string message)
+        {
+            if (_handlers.ContainsKey(eventName))
+            {
+                //Gets everyone who subscribes to the eventName
+                var subscriptions = _handlers[eventName];
+                foreach ( var subsciption in subscriptions)
+                {
+                    //Creates a new instance of the subscription type
+                    var handler = Activator.CreateInstance(subsciption);
+                    if (handler == null) continue;
+                    //Gets the type of event i.e. TransferCreatedEvent
+                    var eventType = _eventTypes.SingleOrDefault(t => t.Name == eventName);
+                    var @event = JsonConvert.DeserializeObject(message, eventType);
+                    var concreteType = typeof(IEventHandler<>).MakeGenericType(eventType);
+                    //Invokes the Handle Method in the correct EventHandler with our new event
+                    await (Task)concreteType.GetMethod("Handle").Invoke(handler, new object[] { @event });
+                }
+            }
         }
     }
 }
