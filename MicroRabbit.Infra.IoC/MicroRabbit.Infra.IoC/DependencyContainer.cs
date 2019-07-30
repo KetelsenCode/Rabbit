9b8ce0501115a6460a00fc26ab306a7343f9e44a
KetelsenCode <pardonmychessmate@gmail.com>
2019-07-30 17:12:07 +0200

    First part of Banking Microservice

--- MicroRabbit.Infra.IoC/MicroRabbit.Infra.IoC/DependencyContainer.cs
+++ MicroRabbit.Infra.IoC/MicroRabbit.Infra.IoC/DependencyContainer.cs
@@ -1,4 +1,9 @@
-﻿using MicroRabbit.Domain.Core.Bus;
+﻿using MicroRabbit.Banking.Application.Interfaces;
+using MicroRabbit.Banking.Application.Services;
+using MicroRabbit.Banking.Data.Context;
+using MicroRabbit.Banking.Data.Repository;
+using MicroRabbit.Banking.Domain.Interfaces;
+using MicroRabbit.Domain.Core.Bus;
 using MicroRabbit.Infra.Bus;
 using Microsoft.Extensions.DependencyInjection;
 using System;
@@ -16,6 +21,10 @@ namespace MicroRabbit.Infra.IoC
 
             //Application Services
             services.AddTransient<IAccountService, AccountService>();
+
+            //Data
+            services.AddTransient<IAccountRepository, AccountRepository>();
+            services.AddTransient<BankingDbContext>();
         }
     }
 }
